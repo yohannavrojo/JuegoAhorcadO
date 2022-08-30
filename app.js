9ceecb0141d72599367c487b4020279b513c1171
yohannavrojo <yohavale7883@gmail.com>
2022-08-30 09:46:45 -0300

    juego ahorcado

---
+++ app.js
@@ -0,0 +1,48 @@
+(function () {
+    'use strict'
+  var juego = {
+    palabra: 'ALURA',
+    estado: 1,
+    adivinado: ['A', 'L'],
+    errado: ['B', 'J', 'K', 'C'],
+  };
+  var $html = {
+    hombre: document.getElementById("hombre"),
+    adivinado: document.querySelector(".adivinado"),
+    errado: document.querySelector(".errado"),
+  };
+  function dibujar(juego) {
+    // actualizar la imagen del hombre
+    var $elem;
+    $elem = $html.hombre;
+    $elem.src = "./imagn/0" + juego.estado + ".png";
+
+    // creamos las letras adivinadas
+    var palabra = juego.palabra;
+    var adivinado = juego.adivinado;
+    $elem = $html.adivinado
+    for (let letra of palabra) {
+       let $span = document.createElement('span')
+       let $txt = document.createTextNode('')
+      if (adivinado.indexOf(letra) >= 0) {
+            $txt.nodeValue=letra
+      }
+      $span.setAttribute('class', 'letra adivinada')
+      $span.appendChild($txt)
+      $elem.appendChild($span)
+    }
+    // creamo la letras erradas
+    var errado = juego.errado
+    $elem =$html.errado
+    for (let letra of errado) {
+       let  $span = document.createElement('span')
+      let  $txt = document.createTextNode(letra) 
+      $span.setAttribute('class', 'letra errada')
+       $span.appendChild($txt)
+       $elem.appendChild($span)
+        
+    }
+  }
+  console.log(juego);
+  dibujar(juego);
+})();
